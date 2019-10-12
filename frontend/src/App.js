9809df2e53bcec711ab3d903c57f59ff2b78b468
LJaglin <ljaglinski@o2.pl>
2019-10-12 22:53:16 +0200

    My first route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-     <h1>Welcome</h1>
-    </div>
+    <Router>
+      <Route path="/home" render={() => console.log('WELCOME')} />
+    </Router>
   );
 }
 
