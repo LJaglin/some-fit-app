6bcd19f2746c9079baaf0db0505250b62ab09d22
LJaglin <ljaglinski@o2.pl>
2019-11-10 23:52:58 +0100

    Create schema for meal

---
+++ backend/models/meal.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const mealSchema = new Schema({
+    description: {
+        type: String,
+        require: true
+    },
+    calorie : {
+        type: Number,
+        required: true
+    },
+    protein : {
+        type: Number,
+        required: true
+    },
+    fat : {
+        type: Number,
+        required: true
+    },
+    carb : {
+        type: Number,
+        required: true
+    }
+});
+
